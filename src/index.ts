1721591897d6b233e95c455066bf000081fd8f30
lbwa <mr_lbw@outlook.com>
2019-08-13 20:04:38 +0800

    feat: declare vue vue-router as peerDependencies
    DOC: https://nodejs.org/en/blog/npm/peer-dependencies/

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,13 @@
-export default {}
+import { VueConstructor } from 'vue'
+import VueRouter from 'vue-router'
+// import VAccess from './components/VAccess'
+
+export default {
+  install(Vue: VueConstructor, { router }: { router: VueRouter }) {
+    // Register global Vue.js component
+    // Vue.component(VAccess.name, Vue.extend(VAccess))
+
+    // router beforeEach navigation guard
+    router.beforeEach((to, _, next) => {})
+  }
+}
