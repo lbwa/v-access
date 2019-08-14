d9a006106f5253b3d19e7f766c7506ae1b362995
lbwa <mr_lbw@outlook.com>
2019-08-14 14:30:23 +0800

    build(examples): fix webpack-dev-server bug
    GUIDE: https://github.com/webpack/webpack-dev-server/issues/2190#issuecomment-520670599

--- examples/vue.config.js
+++ examples/vue.config.js
@@ -1,6 +1,10 @@
 const { resolve } = require('path')
 
 module.exports = {
+  devServer: {
+    // https://github.com/webpack/webpack-dev-server/issues/2190#issuecomment-520670599
+    contentBase: './public'
+  },
   chainWebpack(webpackConf) {
     webpackConf.resolve.alias.set('@src', resolve(__dirname, '../src'))
   }
