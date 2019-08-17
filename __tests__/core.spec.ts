28ff8abb64e29a3ae8b63cff78e089cd05e75dfa
lbwa <mr_lbw@outlook.com>
2019-08-17 19:57:44 +0800

    test(core):  add 'createPrivateRoutes ' and 'rest' test

--- __tests__/core.spec.ts
+++ __tests__/core.spec.ts
@@ -4,17 +4,14 @@ describe('VAccessCore', () => {
   const auth = new VAccessCore()
   const ACCESS_LIST = [
     {
-      id: 'accessOne'
+      id: 'home.read'
     },
     {
-      id: 'accessTwo'
-    },
-    {
-      id: 'accessThree'
+      id: 'service.read'
     }
   ]
 
-  it("With 'new' keyword", () => {
+  it('Instantiation', () => {
     expect(auth.has).toBeDefined()
     expect(auth.init).toBeDefined()
     expect(auth.reset).toBeDefined()
@@ -26,7 +23,7 @@ describe('VAccessCore', () => {
     expect(auth.verifyRouteAccess).toBeDefined()
   })
 
-  it('Init with access list', () => {
+  it('Accept access list and create access map', () => {
     auth.init(ACCESS_LIST)
     expect(auth.map).toBeDefined()
     expect(Object.keys(auth.map).length).toBe(ACCESS_LIST.length)
@@ -34,15 +31,15 @@ describe('VAccessCore', () => {
 
   it('Has ONE access', () => {
     expect(auth.has(ACCESS_LIST[1].id)).toBeTruthy()
-    expect(auth.has('accessFour')).toBeFalsy()
+    expect(auth.has('mongo.read')).toBeFalsy()
   })
 
   it('Has AT LEAST ONE access', () => {
     const PENDING_LIST_TRUTHY = [
-      ['accessOne', 'accessFour'],
-      ['accessOne', 'accessFive']
+      ['home.read', 'mongo.read'],
+      ['home.read', 'sql.read']
     ]
-    const PENDING_FALSY = ['accessFour', 'accessFive']
+    const PENDING_FALSY = ['mongo.read', 'sql.read']
     for (const pending of PENDING_LIST_TRUTHY) {
       expect(auth.weak(pending)).toBeTruthy()
       expect(auth.weakList(pending)).toBeTruthy()
@@ -52,8 +49,8 @@ describe('VAccessCore', () => {
   })
 
   it('Has ALL access', () => {
-    const PENDING_LIST_TRUTHY = [['accessOne', 'accessTwo']]
-    const PENDING_FALSY = ['accessOne', 'accessFour']
+    const PENDING_LIST_TRUTHY = [['home.read', 'service.read']]
+    const PENDING_FALSY = ['home.read', 'mongo.read']
 
     for (const pending of PENDING_LIST_TRUTHY) {
       expect(auth.strict(pending)).toBeTruthy()
@@ -61,5 +58,148 @@ describe('VAccessCore', () => {
     }
     expect(auth.strict(PENDING_FALSY)).toBeFalsy()
     expect(auth.strictList(PENDING_FALSY)).toBeFalsy()
+  })
+
+  // notice: omit all components
+  const routesRoot = {
+    path: '/',
+    meta: {
+      access: ['home.read']
+    }
+  }
+  const routesServiceRead = {
+    path: '/service',
+    meta: {
+      access: ['service.read']
+    }
+  }
+  const routesMongoRead = {
+    path: '/mongo',
+    meta: {
+      access: ['mongo.read']
+    }
+  }
+  const routesServiceOrMongoRead = {
+    path: '/service_mongo',
+    meta: {
+      weakAccess: ['service.read', 'mongo.read']
+    }
+  }
+  const routesNestWithWeakAccess = {
+    path: '/nest_with_weak_access',
+    children: [
+      {
+        path: 'service_or_mongo',
+        weakAccess: ['service.read', 'mongo.read']
+      }
+    ]
+  }
+
+  it('Create private routes with `access` field', () => {
+    expect(
+      auth.createPrivateRoutes([routesRoot, routesServiceRead, routesMongoRead])
+    ).toStrictEqual([routesRoot, routesServiceRead])
+  })
+
+  it('Create private routes with `weakAccess` field', () => {
+    expect(
+      auth.createPrivateRoutes([
+        routesRoot,
+        routesServiceRead,
+        routesMongoRead,
+        routesServiceOrMongoRead
+      ])
+    ).toStrictEqual([routesRoot, routesServiceRead, routesServiceOrMongoRead])
+  })
+
+  it('Create private children routes with `access` field', () => {
+    expect(
+      auth.createPrivateRoutes([
+        {
+          path: '/nest_with_access',
+          // Level 1 nested routes
+          children: [
+            {
+              path: 'service',
+              meta: {
+                access: ['service.read']
+              }
+            },
+            {
+              path: 'mongo',
+              meta: {
+                access: ['mongo.read']
+              }
+            },
+            // Level 2 nested routes
+            {
+              path: 'nest',
+              children: [
+                {
+                  path: 'nest_with_access',
+                  meta: {
+                    access: ['service.read']
+                  }
+                },
+                {
+                  path: 'nest_with_access_mongo',
+                  meta: {
+                    access: ['mongo.read']
+                  }
+                },
+                {
+                  path: 'nest_with_weak_access',
+                  meta: {
+                    weakAccess: ['service', 'service.read']
+                  }
+                }
+              ]
+            }
+          ]
+        }
+      ])
+    ).toStrictEqual([
+      {
+        path: '/nest_with_access',
+        children: [
+          {
+            path: 'service',
+            meta: {
+              access: ['service.read']
+            }
+          },
+          {
+            path: 'nest',
+            children: [
+              {
+                path: 'nest_with_access',
+                meta: {
+                  access: ['service.read']
+                }
+              },
+              {
+                path: 'nest_with_weak_access',
+                meta: {
+                  weakAccess: ['service', 'service.read']
+                }
+              }
+            ]
+          }
+        ]
+      }
+    ])
+  })
+
+  it('Create private children routes with `weakAccess` field', () => {
+    expect(auth.createPrivateRoutes([routesNestWithWeakAccess])).toStrictEqual([
+      routesNestWithWeakAccess
+    ])
+  })
+
+  it('Reset all data', () => {
+    auth.reset()
+    expect(auth.map).toEqual({})
+    expect(auth.created).toBe(false)
+    expect(auth.size).toBe(0)
   })
 })
